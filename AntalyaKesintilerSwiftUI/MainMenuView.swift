fa25e8cad75d1be0042b8d2b0564b1b9d5a0a6c6
Yasin Şükrü Tan <yasinsukrutan@gmail.com>
2020-07-23 20:51:49 +0300

    First commit

---
+++ AntalyaKesintilerSwiftUI/MainMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MainMenuView.swift
+//  AntalyaKesintilerSwiftUI
+//
+//  Created by Yasin Şükrü Tan on 23.07.2020.
+//  Copyright © 2020 Yasin Şükrü Tan. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
